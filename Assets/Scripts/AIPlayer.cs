ebe699350e76e3f314323930e611cbbab607b417
Chris D <chris@deweese.com>
2022-12-11 17:53:32 -0600

    Create AIPlayer.cs

---
+++ Assets/Scripts/AIPlayer.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class AIPlayer : MonoBehaviour
+{
+    private Vector2 _location;
+    private float _timer;
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= 5.0f)
+        {
+            _timer = 0f;
+            CalculateNextMove();
+        }
+    }
+
+    private void CalculateNextMove()
+    {
+        int randomMoveChoice = Random.Range(0, 3);
+        
+        switch (randomMoveChoice)
+        {
+            case 0:
+                MoveToNearestAmmo();
+                break;
+            case 1:
+                MoveToNearestCannon();
+                break;
+            case 2:
+                MoveToNearestDamage();
+                break;
+            case 3:
+                MoveToIdle();
+                break;
+        }
+    }
+
+    private void MoveToNearestAmmo()
+    {
+        
+    }
+
+    private void MoveToNearestCannon()
+    {
+        
+    }
+
+    private void MoveToNearestDamage()
+    {
+        
+    }
+
+    private void MoveToIdle()
+    {
+        // do nothing and wait next turn
+    }
+}
